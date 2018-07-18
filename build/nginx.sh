756354e41c91aa045d350d7cfa7b22bcc5b57c63
CodeMonkeyJeffGT <18846938586@163.com>
2018-07-18 20:15:40 +0800

    nginx 安装脚本

--- build/nginx.sh
+++ build/nginx.sh
@@ -3,6 +3,19 @@
 echo 安装\ nginx
 cd nginx
 
+if [ $1 ] ; then
+    loc=$1
+else
+    echo -n 请输入安装路径，默认为\ /env/nginx/：
+    read loc
+    if [ ! $loc ] ; then
+        loc=/env/nginx/
+    fi
+fi
 
+yum -y install openssl-devel
+./configure --prefix=$loc --with-http_stub_status_module --with-http_ssl_module
+make
+make install
 
 cd ../
